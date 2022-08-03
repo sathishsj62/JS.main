eca5cae27b9d4855502ed586412eab67360457d3
SathishKumar <sathishvivis6620@gmail.com>
2022-08-03 14:23:49 +0530

    modified:index.html

---
+++ sathis.js
@@ -0,0 +1,56 @@
+// ADDITION 
+let a = 50;
+let b = 30;
+let c = a + b;
+
+// console.log(c)
+
+let x = 20;
+let y = 10;
+let z = x + y;
+
+// console.log(z);
+
+// Subtraction
+
+let d = 40;
+let e = 20;
+let f = d - e;
+
+// console.log(f);
+
+let g = 100;
+let h = 17;
+let i = g - h;
+
+// console.log(i);
+
+// Multiplication 
+
+let j = 40;
+let k = 35;
+let l = j * k;
+
+// console.log(l);
+
+let m = 15;
+let n = 10;
+let o = m * n;
+
+// console.log(o);
+
+//Division
+let p = 15;
+let q =10;
+let r = p / q ;
+
+console.log(r);
+
+let s = 10;
+let u = 10;
+let v = s / u;
+
+console.log(v);
+
+
+
