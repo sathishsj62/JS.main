ed5478abf0aef5bafcf28ffdb9eb0aae3f95742b
SathishKumar <sathishvivis6620@gmail.com>
2022-08-05 17:39:12 +0530

    modified:index.html

--- addition.js
+++ addition.js
@@ -6,5 +6,12 @@ let sum;
 
 sum = a - (b - (c - (c * (-b) + d) + b - a));
 
-console.log(sum);
+// console.log(sum);
 
+const width = 3 ;
+const height = 4 ;
+const getRectArea = function(width,height){
+    return width * height;
+
+};
+console.log(getRectArea(width,height));
---
+++ function.js
@@ -0,0 +1,11 @@
+  function hello(moi) {
+    if(moi < 100){
+        return "Banana";
+    }
+    else{
+        return "bad";
+    }
+    
+  }
+let x = hello(101);
+console.log(x);
