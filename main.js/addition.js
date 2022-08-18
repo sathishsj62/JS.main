d04ba503772b0384c2192992e478771c54f23c09
SathishKumar <sathishvivis6620@gmail.com>
2022-08-18 15:38:49 +0530

    modified:index.html

--- main.js/addition.js
+++ main.js/addition.js
@@ -1,16 +1,25 @@
 // ADDITION //
-let a = 50;
-let b = 30;
-let c = a + b;
+// let a = 50;
+// let b = 30;
+// let c = a + b;
 
- console.log(c)
+//  console.log(c)
 
 
- let v,w,x,y,z;
- v = 50;
- w = 60;
- x = 30;
- y = 35;
- z = 25;
- let u = v+w+x+y+z;
- console.log(u);
+//  let v,w,x,y,z;
+//  v = 50;
+//  w = 60;
+//  x = 30;
+//  y = 35;
+//  z = 25;
+//  let u = v+w+x+y+z;
+//  console.log(u);
+
+
+
+ 
+ let d = new Date(); 
+
+var  k = d.setDate(23);
+console.log(k);
+document.getElementById('sample').innerHTML=d;
--- main.js/splice.js
+++ main.js/splice.js
@@ -1,6 +1,8 @@
-var a1 =["sathish","rabhul bro","vicky bro","barath"];
-var a2 =["a","b","c","d","e"];
+// var a1 =["sathish","rabhul bro","vicky bro","barath"];
+// var a2 =["a","b","c","d","e"];
 
 
- console.log(a1.splice(1));
- console.log(a2.splice(2));
+//  console.log(a1.splice(1));
+//  console.log(a2.splice(2));
+
+
