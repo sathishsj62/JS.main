79e413f0dc7a8e41168550e9f4ad8ccd7ddc6624
SathishKumar <sathishvivis6620@gmail.com>
2022-08-20 18:07:49 +0530

    modified:index.html

--- main.js/Area of Circle .js
+++ main.js/Area of Circle .js
@@ -1,5 +1,12 @@
-let r = 10;
-let s = 3.14
-let u = s*r*r ;
+// let r = 10;
+// let s = 3.14
+// let u = s*r*r ;
 
-console.log(s);
+// console.log(s);
+
+
+
+let a = 10;
+let c = Math.PI*(a*a);
+
+console.log(c);
