8896a24f4a1a60236f14a435a4f78a9c86a5f916
SathishKumar <sathishvivis6620@gmail.com>
2022-08-22 23:18:41 +0530

    modified:index.html

---
+++ main.js/reverse order.js
@@ -0,0 +1,26 @@
+// for loop //
+
+
+// let x=5;
+// for(i=x;i>0;i--){
+//     console.log(i)
+// }
+    
+// let a =100;
+// for(i=a;i>0;i--){
+//     console.log(i)
+// }
+
+// while loop //
+
+// let x = 10;
+// while(x>=0){
+//     console.log(x);
+//     x--;
+// }
+
+let a =100;
+while(a>0){
+    console.log(a);
+    a--
+}
