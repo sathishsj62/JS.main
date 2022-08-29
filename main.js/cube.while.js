9a3dba5778511c696ede509848f69cec0da5959c
SathishKumar <sathishvivis6620@gmail.com>
2022-08-29 15:09:18 +0530

    modified:index.html

---
+++ main.js/cube.while.js
@@ -0,0 +1,11 @@
+let a = prompt("enter the value"); 
+let b =0 , c = 1;
+while(b<=a){
+	k=(b**3);
+	c=k;
+	console.log(c);
+	b++;
+}
+
+
+
---
+++ main.js/table. multi.js
@@ -0,0 +1,25 @@
+var num=1;
+for(let i=1;i<=10;i++){
+   var result=i*num;
+   console.log(i+"*"+num+"="+result);
+}
+
+let a = 2 ;
+for(let i = 1 ; i<=10; i++){
+    let b = i*a;
+    console.log(i+"*"+a+"="+b);
+}
+
+let b = 3;
+for(let i = 1; i<=10; i++){
+    let c = i*b;
+    console.log(i+"*"+b+"="+c);
+}
+ 
+
+let c = 4;
+for(let i = 1;i<=10; i++){
+    let d = i*c ;
+    console.log(i+"*"+c+"="+d);
+}
+
