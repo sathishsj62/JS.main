45a870897edde7f5d0f2d74dfb5317d7281db288
SathishKumar <sathishvivis6620@gmail.com>
2022-09-02 11:16:21 +0530

    modified:index.html

--- main.js/Armstrong or not.js
+++ main.js/Armstrong or not.js
@@ -1,4 +1,20 @@
-let x=371;
+// let x=371;
+// let y=x.toString();
+// let sum=0;
+// for(i=y.length-1;i>=0;i--){
+//     let b=parseInt(y[i]);
+//     sum+=b*b*b;
+// }
+// if(sum==x){
+//     console.log("amstrong number",x);
+// }
+// else{
+//     console.log('not amstrong number',x);
+// }
+
+// not amstrong num/// 
+
+let x=357;
 let y=x.toString();
 let sum=0;
 for(i=y.length-1;i>=0;i--){
