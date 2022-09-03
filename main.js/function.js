837f0f32b935afa099b6b5f5851b88e05b6624fc
SathishKumar <sathishvivis6620@gmail.com>
2022-09-03 21:14:28 +0530

    modified:index.html

--- main.js/function.js
+++ main.js/function.js
@@ -101,3 +101,51 @@ let num = (a,b,c) => a+b+c ;
 console.log(num(10,20,30));
 
 
+// sum of numbers
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum = sum + i;
+
+}
+console.log(sum);
+
+
+// factorial
+let factorial = 1, number = 5;
+for (let i = number; i >= 1; i--) {
+    factorial = factorial * i;
+}
+console.log(factorial);
+
+// 
+let fact = 1;
+let f4 = 5;
+for (let i = f4; i >=1; i--) {
+    fact = fact * i;
+}
+console.log(fact);
+
+// ascending order using for loop
+function value() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+value();
+
+// ascending order using while loop
+
+function value() {
+    let i = 1;
+    while (i <= 10) {
+        console.log(i);
+        i++
+    }
+}
+value();
+// decending order using for loop
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
