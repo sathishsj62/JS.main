50986571648363a72a50f576cc9b43601118d785
SathishKumar <sathishvivis6620@gmail.com>
2022-09-08 18:29:16 +0530

    modified:index.html

--- main.js/function.js
+++ main.js/function.js
@@ -1,60 +1,60 @@
 //// function direct call method ////
 
-function add(a,b) {
-    console.log(a+b);
-    
-}
-add(40,30);
+// function add(a,b) {
+//     console.log(a+b);
+
+// }
+// add(40,30);
 
 // function add with variable//
 
-function add(a,b){
-  let c =a+b;
-  console.log(c);
+// function add(a,b){
+//   let c =a+b;
+//   console.log(c);
 
-}
-add(10,15);  
+// }
+// add(10,15);  
 
 // function  even num//
-function odd (a){
-  for(i=0 ; i<a ; i++){
-    if(i%2==0){
-      return"even"
-    }
-  }
- 
-}
-odd(10);
+// function odd (a){
+//   for(i=0 ; i<a ; i++){
+//     if(i%2==0){
+//       return"even"
+//     }
+//   }
 
-  
+// }
+// odd(10);
+
+
 //function using condition//
 
-function hello(moi) {
-    if(moi < 100){
-        return "Banana";
-    }
-    else{
-        return "bad";
-    }
+// function hello(moi) {
+//     if(moi < 100){
+//         return "Banana";
+//     }
+//     else{
+//         return "bad";
+//     }
 
-}
-let x = hello(101);
-console.log(x);
+// }
+// let x = hello(101);
+// console.log(x);
 
 
-function name (vivi){
-  if(vivi<100){
-    return"no"
+// function name (vivi){
+//   if(vivi<100){
+//     return"no"
 
-  }else if (vivi<=160 && vivi>=100){
-    return"yes"
+//   }else if (vivi<=160 && vivi>=100){
+//     return"yes"
 
-  }else{
-    return"equal"
-  }
-}
-let a = name(prompt("enter the value"));
-console.log(a);
+//   }else{
+//     return"equal"
+//   }
+// }
+// let a = name(prompt("enter the value"));
+// console.log(a);
 
 // // function  //
 // function vowelsAndConsonants(s) {
@@ -73,104 +73,132 @@ console.log(a);
 
 // function odd  & even //
 
-function odd(x){
-  for(i=0; i<x ;i++){
-    if(i%2==0){
-      console.log("even:"+i);
-    }
-    else{
-      console.log("odd:"+i);
-    }
+// function odd(x){
+//   for(i=0; i<x ;i++){
+//     if(i%2==0){
+//       console.log("even:"+i);
+//     }
+//     else{
+//       console.log("odd:"+i);
+//     }
 
-  }
+//   }
 
-}
-odd(20)
+// }
+// odd(20)
 
 
 ///Arrow function///
 
-let name = a => a * a;
-console.log(name(5));
+// let name = a => a * a;
+// console.log(name(5));
 
 
-let value = (a,b) => a + b;
-console.log(value(5, 40));
+// let value = (a,b) => a + b;
+// console.log(value(5, 40));
 
-let num = (a,b,c) => a+b+c ;
-console.log(num(10,20,30));
+// let num = (a,b,c) => a+b+c ;
+// console.log(num(10,20,30));
 
 
-// sum of numbers
-let sum = 0;
-for (let i = 1; i <= 5; i++) {
-    sum = sum + i;
+// // sum of numbers
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum = sum + i;
 
+// }
+// console.log(sum);
+
+
+// // factorial
+// let factorial = 1, number = 5;
+// for (let i = number; i >= 1; i--) {
+//     factorial = factorial * i;
+// }
+// console.log(factorial);
+
+// // 
+// let fact = 1;
+// let f4 = 5;
+// for (let i = f4; i >=1; i--) {
+//     fact = fact * i;
+// }
+// console.log(fact);
+
+// // ascending order using for loop
+// function value() {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(i);
+//     }
+// }
+// value();
+
+// // ascending order using while loop
+
+// function value() {
+//     let i = 1;
+//     while (i <= 10) {
+//         console.log(i);
+//         i++
+//     }
+// }
+// value();
+// // decending order using for loop
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+// //function tabule//
+
+// // random color //
+
+// function randomcolor(){
+// 	var x = Math.floor(Math.random() * 100);
+// 	var y = Math.floor(Math.random() * 150);
+//     var randomcolor = "rgb(" + x + "," + y + ")";
+
+// 	console.log(randomcolor);
+
+
+// }
+
+// randomcolor();
+
+// // random num //
+
+// function randomnum(){
+//   let a = Math.floor(Math.random()*60)
+//   let b = Math.floor(Math.random()*100)
+//   let c = Math.floor(Math.random()*150)
+//   let randomnum = "num("+ a + "," + b + "," + c +")";
+//   console.log(randomnum);
+// }
+// randomnum();
+
+
+
+/// tow stringvowels//
+
+function x(a,b){
+	let c=0;
+	for(i=0; i<a.length;i++){
+		for(j=0; j<b.length ; j++){
+			if(a[i]==b[j])
+				c++
+		}
+	}
+	return c ;
 }
-console.log(sum);
-
-
-// factorial
-let factorial = 1, number = 5;
-for (let i = number; i >= 1; i--) {
-    factorial = factorial * i;
+let vs,vr;
+let s = prompt("enter the first string");
+let r = prompt("enter the first string");
+let v  =["a","e","i","o","u","A","E","I","O","U"];
+vs = x(s,v);
+vr = x(r,v);
+if(vs==vr){
+	console.log("the both string has same vowels");
+}else if( vs>vr){
+	console.log("the first string has more vowels");
+}else{
+	console.log("the second string has more vowels");
 }
-console.log(factorial);
-
-// 
-let fact = 1;
-let f4 = 5;
-for (let i = f4; i >=1; i--) {
-    fact = fact * i;
-}
-console.log(fact);
-
-// ascending order using for loop
-function value() {
-    for (let i = 1; i <= 10; i++) {
-        console.log(i);
-    }
-}
-value();
-
-// ascending order using while loop
-
-function value() {
-    let i = 1;
-    while (i <= 10) {
-        console.log(i);
-        i++
-    }
-}
-value();
-// decending order using for loop
-
-for (let i = 10; i >= 1; i--) {
-    console.log(i);
-}
-//function tabule//
-
-// random color //
-
-function randomcolor(){
-	var x = Math.floor(Math.random() * 100);
-	var y = Math.floor(Math.random() * 150);
-    var randomcolor = "rgb(" + x + "," + y + ")";
-	
-	console.log(randomcolor);
-	
-	
-}
-
-randomcolor();
-
-// random num //
-
-function randomnum(){
-  let a = Math.floor(Math.random()*60)
-  let b = Math.floor(Math.random()*100)
-  let c = Math.floor(Math.random()*150)
-  let randomnum = "num("+ a + "," + b + "," + c +")";
-  console.log(randomnum);
-}
-randomnum();
